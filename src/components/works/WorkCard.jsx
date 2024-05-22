0a51b537a108e08567d0f3b145612f1088361550
raju0005 <anderajesh15@gmail.com>
2024-05-22 15:12:15 +0530

    im

--- src/components/works/WorkCard.jsx
+++ src/components/works/WorkCard.jsx
@@ -6,7 +6,7 @@ export const WorkCard = (work) => {
   return (
     <div className='border border-[rgba(_205,_210,_230,_0.21)] flex flex-col p-3 flex-1 justify-around items-center rounded-xl h-[310px] bg-primary sm:max-w-[15%] max-w-[60%] m-3  gap-[20px] sm:gap-1 shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px] animate__animated animate__headShake'>
       <div className='w-[150px] sm:w-[190px] relative'>
-        <img className='object-contain rounded-lg' src={work.logo} alt={work.title} />
+        <img className='sm:object-contain object-fill rounded-lg' src={work.logo} alt={work.title} />
         <div className='w-full h-full flex justify-center items-center absolute inset-0 rounded-lg bg-black/60 opacity-0 hover:opacity-100 transition-all duration-500'>
           <a href={work.link} className='flex justify-center items-center w-[19%] m-5'>
             <img className='object-contain' src={eye} alt="View" />
