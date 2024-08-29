506f5230fec8cfc74efd20e82b9e65d22a9267db
raju0005 <anderajesh15@gmail.com>
2024-08-29 20:27:56 +0530

    netlify

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "";
-export const MESSAGES_URL = "/api/rajesh";
-export const CONTACT_URL = "/api/contact";
+export const MESSAGES_URL = "/api/fitass/rajesh";
+export const CONTACT_URL = "/api/fitass/contact";
 
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api/fitass": "https://fitass-server.vercel.app",
     }
   }
 })
