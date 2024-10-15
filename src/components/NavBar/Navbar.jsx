e1ba39918bfd745e3e3605cad1006177123dc419
raju0005 <anderajesh15@gmail.com>
2024-10-15 12:56:40 +0530

    some dimensions

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
     <div className=" w-screen z-10 ">
       <ul className='flex p-[2px] w-full justify-around gap-[10px] bg-primary items-center rounded-xl shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px]'>
         <div className='max-w-[50%] px-[25px] md:max-w-[80%] md:px-[10px] flex justify-center md:justify-start items-center'>
-          <img className='md:max-w-[40%] md:h-[70px] h-[60px] w-[140px] object-contain animate__animated animate__headShake ' src={logo} alt="" />
+          <img className='md:w-[170px] md:h-[70px] h-[60px] w-[140px] object-contain animate__animated animate__headShake ' src={logo} alt="" />
         </div>
         <div className='flex flex-1 justify-center md:gap-8 gap-4 p-[3px]  sm:max-w-[15%] items-center'>
           <a href="https://www.linkedin.com/in/rajeshande2004/"><img src={linkedin} alt="" className='hover:scale-110 rounded transition-transform transform animate__animated animate__headShake md:w-[40px] md:h-[40px] h-[30px] w-[30px] object-contain' /></a>
