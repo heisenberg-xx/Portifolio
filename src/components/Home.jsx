2d5170fadf8c720b8a0adc0bbee19081db05febc
raju0005 <anderajesh15@gmail.com>
2024-10-15 12:53:01 +0530

    Ui changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,12 @@ import 'animate.css'
 
 export const Home = () => {
     return (
-        <div id="home" className='reveal  flex flex-col sm:flex-row m-0 gap-6 justify-around sm:justify-center items-center flex-1 z-0 rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] '>
-            
+        <div id="home" className='reveal pt-20 flex flex-col sm:flex-row  gap-6 justify-around sm:justify-center items-center flex-1  rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] '>
             <div className='flex flex-1 font-font1 mt-[50px] flex-grow flex-col animate__animated animate__headShake gap-[10px] sm:h-[50vh] sm:justify-start justify-center sm:items-start items-start max-w-[70%] sm:max-w-[30%]'>
-
                 <span className='font-bold font-font2 text-[20px] text-text1 sm:text-[20px]'>Hello, its me</span>
-                <span className='  text-[50px] text-text1 font-customPD sm:text-[70px]'>Rajesh <span className=' *:'>.</span></span>
+                <span className='  text-[50px] text-text1 font-customPD sm:text-[70px]'>Rajesh <span className=''>.</span></span>
                 <span><p className='text-gray-600 font-font2'>An enthusiastic web developer currently shapin the future of webdevelopment by designing the smooth user-interfaces that promote user-interaction with information and data </p></span>
-                < a href="https://drive.google.com/file/d/1VZFtGlvd4NY7u-11CZPDZYaE-ih7Up5v/view?usp=drive_link">
+                <a href="https://drive.google.com/file/d/1VZFtGlvd4NY7u-11CZPDZYaE-ih7Up5v/view?usp=drive_link">
                     <button className="px-4 py-3 hover:bg-white/70 transition-all duration-150 gap-2 rounded-md my-10 border flex justify-around items-center  border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] ">
                         <span className='font-bold font-font2'>My Resume</span><img className='w-[18px]' src={download} alt="" />
                     </button>
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
 import 'animate.css';
 import logo from '../../assets/logo.svg';
-
 import github from '../../assets/github.svg';
 import linkedin from '../../assets/linkedin.svg';
+import profile from '../../assets/2.jpg'
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
 
   return (
-    <div className="relative z-10">
-      <ul className='flex p-2 w-full justify-around gap-[60px] bg-primary items-center rounded-xl shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px]'>
-        <div className='sm:flex hidden m-7 sm:max-w-[40%] sm:px-[25px] max-w-[45%] px-[10px]'>
+    <div className=" w-screen z-10 ">
+      <ul className='flex p-[2px] w-full justify-around gap-[10px] bg-primary items-center rounded-xl shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px]'>
+        <div className='max-w-[50%] px-[25px] md:max-w-[80%] md:px-[10px] flex justify-center md:justify-start items-center'>
+          <img className='md:max-w-[40%] md:h-[70px] h-[60px] w-[140px] object-contain animate__animated animate__headShake ' src={logo} alt="" />
         </div>
-        <div className='sm:max-w-[50%] sm:px-[25px] max-w-[40%] px-[10px] flex justify-center items-center'>
-          <img className='sm:max-w-[40%] object-contain animate__animated animate__headShake  h-[85px]' src={logo} alt="" />
-        </div>
-        <div className='flex flex-1 justify-center gap-8 p-[10px]  sm:max-w-[15%] items-center'>
-          <a href="https://www.linkedin.com/in/rajeshande2004/"><img src={linkedin} alt="" className='hover:scale-110 rounded transition-transform transform animate__animated animate__headShake w-[35px] h-[35px]   object-contain' /></a>
-          <a href="https://github.com/raju0005"><img src={github} alt="" className='hover:scale-110 rounded transition-transform transform w-[35px] h-[35px] animate__animated animate__headShake object-contain' /></a>
-         
+        <div className='flex flex-1 justify-center md:gap-8 gap-4 p-[3px]  sm:max-w-[15%] items-center'>
+          <a href="https://www.linkedin.com/in/rajeshande2004/"><img src={linkedin} alt="" className='hover:scale-110 rounded transition-transform transform animate__animated animate__headShake md:w-[40px] md:h-[40px] h-[30px] w-[30px] object-contain' /></a>
+          <a href="https://github.com/raju0005"><img src={github} alt="" className='hover:scale-110 rounded transition-transform transform md:w-[40px] md:h-[40px] h-[30px] w-[30px] animate__animated animate__headShake object-contain' /></a>
+          <div className='md:w-[40px] md:h-[40px] h-[30px] w-[30px]flex justify-center items-center'><img className='h-full w-full object-fill rounded border-2 border-white' src={profile} /></div>
         </div>
       </ul>
     </div>
