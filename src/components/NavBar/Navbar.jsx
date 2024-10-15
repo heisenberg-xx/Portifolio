8014aff1c49db16e047556ec6bf2ad58a0918da1
raju0005 <anderajesh15@gmail.com>
2024-10-15 11:58:48 +0530

    hu changes

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import 'animate.css';
 import logo from '../../assets/logo.svg';
-import menu from '../../assets/down.svg';
-import close from '../../assets/up.svg';
-import { Navdrop } from './Navdrop';
+
 import github from '../../assets/github.svg';
 import linkedin from '../../assets/linkedin.svg';
 
@@ -13,26 +11,16 @@ export const Navbar = () => {
 
   return (
     <div className="relative z-10">
-      <ul className='flex p-2 w-full justify-evenly gap-[60px] bg-primary items-center rounded-xl shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px]'>
+      <ul className='flex p-2 w-full justify-around gap-[60px] bg-primary items-center rounded-xl shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px]'>
         <div className='sm:flex hidden m-7 sm:max-w-[40%] sm:px-[25px] max-w-[45%] px-[10px]'>
         </div>
         <div className='sm:max-w-[50%] sm:px-[25px] max-w-[40%] px-[10px] flex justify-center items-center'>
           <img className='sm:max-w-[40%] object-contain animate__animated animate__headShake  h-[85px]' src={logo} alt="" />
         </div>
-        <div className='flex flex-1 justify-around p-[10px] gap-[20px] sm:max-w-[15%] items-center'>
-          <a  href="https://www.linkedin.com/in/rajeshande2004/"><img src={linkedin} alt="" className='hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(255,255,255,0.3)_0px_3px_7px_-3px] rounded transition duration-300 animate__animated animate__headShake w-[30px] h-[30px]   object-contain' /></a>
-          <a href="https://github.com/raju0005"><img src={github} alt="" className='hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] rounded transition duration-300 w-[30px] h-[30px] animate__animated animate__headShake object-contain' /></a>
-          <img
-            src={toggle ? close : menu}
-            alt="Menu"
-            className={`animate__animated animate__headShake w-[20px] h-[20px] object-contain cursor-pointer`}
-            onClick={() => setToggle((prev) => !prev)}
-          />
-          <div
-            className={`${toggle ? 'flex animate__animated animate__slideInDown ' : 'hidden '} p-6 shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px] bg-primary absolute text-text1 top-[90px] sm:right-[80px] right-[-1px] mx-4 my-3 sm:w-[400px] w-[370px] rounded-xl z-[-2] border border-[rgba(_205,_210,_230,_0.21)]`}
-          >
-            <Navdrop />
-          </div>
+        <div className='flex flex-1 justify-center gap-8 p-[10px]  sm:max-w-[15%] items-center'>
+          <a href="https://www.linkedin.com/in/rajeshande2004/"><img src={linkedin} alt="" className='hover:scale-110 rounded transition-transform transform animate__animated animate__headShake w-[35px] h-[35px]   object-contain' /></a>
+          <a href="https://github.com/raju0005"><img src={github} alt="" className='hover:scale-110 rounded transition-transform transform w-[35px] h-[35px] animate__animated animate__headShake object-contain' /></a>
+         
         </div>
       </ul>
     </div>
--- src/components/works/WorkCard.jsx
+++ src/components/works/WorkCard.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 export const WorkCard = ({ work }) => {
   return (
-    <div className="border border-[rgba(_205,_210,_230,_0.21)] flex flex-row px-5 py-3  flex-1 justify-around items-center rounded-xl bg-primary md:max-w-[62%] w-[90%] gap-7 shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px] animate__animated animate__headShake hover:animate-buzz relative md:max-h-[350px]">
-      <div className="w-[200px] hidden md:flex">
+    <div className="border border-[rgba(_205,_210,_230,_0.21)] flex flex-row px-5 py-3  flex-1 justify-around items-center rounded-xl bg-primary md:max-w-[62%] w-[90%] gap-7 shadow-[rgba(_205,_210,_230,_0.21)_0px_3px_8px] animate__animated  hover:animate-buzz relative md:max-h-[350px]">
+      <div className="w-[200px] hidden md:flex ">
         <img
           className="object-fill rounded-lg"
           src={work.logo}
           alt={work.title}
         />
       </div>
-      <div className="w-[150px] md:hidden flex absolute opacity-65 z-10">
+      <div className="w-[150px] md:hidden flex absolute  z-10">
         <img src={work.logo} alt={work.title} className="object-cover" />
       </div>
-      <div className="flex flex-col justify-between items-center w-full gap-2 bg-black/30 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 h-full z-20">
+      <div className="flex flex-col justify-between items-center w-full gap-2 bg-black/70 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 h-full z-20">
         <h1 className=" text-[30px] mt-3 mb-1 font-customPD text-white lowercase">
           {work.title}
         </h1>
-        <ul className="font-font2 flex flex-col justify-around items-start text-gray-500 text-[13px]  pb-2">
+        <ul className="font-font2 flex flex-col justify-around items-start text-gray-400 text-[13px]  pb-2">
           {work.desc.map((item, index) => (
             <li key={index} className="list-disc list-outside">
               {item}
