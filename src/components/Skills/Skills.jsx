65f06e1dfdc62372e61cb74129a26031d5358925
raju0005 <anderajesh15@gmail.com>
2025-02-20 23:11:31 +0530

    New Skill Unlocked

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,6 +4,7 @@ import python from "../../assets/python.svg";
 import express from "../../assets/express.svg";
 import tailwind from "../../assets/tailwind.svg";
 import javascript from "../../assets/javascript.svg";
+import typescript from "../../assets/typescript.svg";
 import redux from '../../assets/redux.svg'
 import lr from '../../assets/lr.svg'
 
@@ -18,10 +19,11 @@ export const Skills = () => {
       <h1 className="text-[40px] text-text1">Skills</h1>
       <div className="mt-10 flex flex-wrap justify-center gap-4 md:max-w-[60%] w-full"> {/* Use gap-4 for spacing */}
         <Skillcard logo={python} name="Python" />
+        <Skillcard logo={javascript} name="JavaScript" />
+        <Skillcard logo={typescript} name="TypeScript" />
         <Skillcard logo={react} name="React" />
         <Skillcard logo={express} name="Express" />
         <Skillcard logo={tailwind} name="Tailwind CSS" />
-        <Skillcard logo={javascript} name="JavaScript" />
         <Skillcard logo={redux} name="Redux" />
         <Skillcard logo={lr} name="Lightroom" />
        
