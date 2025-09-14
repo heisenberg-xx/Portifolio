e8d48b192e5e07ae26fdde4eef2e2e6a4450fefb
raju0005 <anderajesh15@gmail.com>
2025-09-14 23:32:18 +0530

    feat :  responsiveness added

--- src/components/works/ComingSoon.jsx
+++ src/components/works/ComingSoon.jsx
@@ -12,27 +12,26 @@ const ComingSoon = () => {
 
   if (!chainReady) {
     return (
-      <div className="flex justify-center items-center min-h-[210px] bg-primary rounded-xl border-2 border-white">
-      </div>
+      <div className="flex justify-center items-center min-h-[210px] bg-primary rounded-xl border-2 border-white"></div>
     );
   }
 
   return (
-    <div className="relative flex flex-col justify-around items-center bg-primary w-auto min-h-[210px] md:max-w-96 px-5 py-3 rounded-xl gap-7 shadow-[rgba(205,210,230,0.21)_0px_3px_8px] border-2 border-white transition-all duration-300 ease-in-out hover:shadow-[rgba(205,210,230,0.5)_0px_5px_15px]">
+    <div className="relative flex flex-col justify-around items-center bg-primary w-auto min-h-[210px] md:max-w-96 px-5 py-3 rounded-xl gap-7 shadow-[rgba(205,210,230,0.21)_0px_3px_8px] border-2 border-white transition-all duration-300 ease-in-out hover:shadow-[rgba(205,210,230,0.5)_0px_5px_15px] overflow-hidden">
       <h1 className="text-5xl text-white z-10">Cooking ...</h1>
 
       <div className="absolute flex items-center w-[115%] z-20">
         <img src={chain} alt="chain" loading="lazy" />
       </div>
-      <div className="absolute flex items-center w-[115%] mb-2 -z-10">
+      {/* <div className="absolute flex items-center w-[115%] mb-2 -z-10">
         <img src={chain} alt="chain" loading="lazy" />
-      </div>
+      </div> */}
       <div className="absolute flex items-center w-[115%] rotate-45 ml-5 mt-2 z-20">
         <img src={chain} alt="chain" loading="lazy" />
       </div>
-      <div className="absolute flex items-center w-[116%] rotate-45 ml-5 mt-3 -z-10">
+      {/* <div className="absolute flex items-center w-[116%] rotate-45 ml-5 mt-3 -z-10">
         <img src={chain} alt="chain" loading="lazy" />
-      </div>
+      </div> */}
     </div>
   );
 };
